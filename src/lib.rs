b441dbc19306923709551a612111c9d51c42e440
vcpersike <vcpersike@gmail.com>
2023-06-27 03:03:13 -0300

    ajustees

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,24 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use gdnative::methods::MethodDecorators;
+use gdnative::prelude::*;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(NativeClass)]
+#[inherit(Node)]
+pub struct MstGames;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[methods]
+impl MstGames {
+    #[export]
+    fn _ready(&self, _owner: &Node) {
+        godot_print!("Hello, world!");
     }
 }
+
+fn mstgames_constructor(_owner: &Node) -> MstGames {
+    MstGames
+}
+
+fn init(handle: InitHandle) {
+    handle.add_class::<MstGames>();
+}
+
+godot_init!(init);
